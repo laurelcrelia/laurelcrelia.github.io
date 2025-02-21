819a169e68876c9a6bc48ce000bc24937ed0e93e
laurelcrelia <laurel.matt@helsinki.fi>
2025-02-21 16:15:37 +0200

    Some tiny css enchancements to Projects and About sections

--- src/pages/Home/components/About.jsx
+++ src/pages/Home/components/About.jsx
@@ -3,37 +3,31 @@ import { TbFileCv } from "react-icons/tb";
 
 const About= () => {
     return (
-      <div className="py-10 text-white bg-[#090909] h-auto "id="about">
-        <div className="flex sm:flex-row items-center px-10 max-w-[1200px] mx-auto">
-            <div>
-                <div className="p-2">
-                    <div className="text-gray-300 my-3">
-                        <h3 className="title-large">About</h3>
-                        <p className="flex items-center justify-center mb-4 text-xl md:text-2xl">
-                            I&apos;m a fourth-year computer science student at the University of Helsinki,
-                            currently pursuing my master&apos;s degree. 
-                            I am open to all possibilities in the tech field, but currently my expertise lies in agile and user-centered software development.
-                        </p>
-                        <p className="flex items-center justify-start text-xl md:text-2xl">
-                        More about my experience in my CV:
-                        </p>
-                        <div className="my-7 sm:justify-center md:justify-start flex flex-col gap-5 text-5xl text-primary sm:flex-row">
-                        <a
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                href="https://drive.google.com/file/d/1rszwL5F5OuKhJV3-JxG8Ge6mbUCCTIsA/view?usp=sharing"
-                                className="button-hover flex items-center justify-center rounded-lg border vibrant-border px-4 py-2 text-lg vibrant-text"
-                                >
-                                <TbFileCv className="mr-2 icon-primary" /> CV
-                            </a>
-                        </div>
-                    </div>
-
-
+        <div className="text-white bg-[#090909] min-h-screen items-center flex sm:flex-row px-10 justify-between"id="about">
+            <div className="max-w-[1200px] mx-auto text-gray-300">
+                <h2 className="title-large pb-8">
+                About
+                </h2>
+                <p className="items-center justify-center mb-4 text-xl md:text-2xl">
+                    I&apos;m a fourth-year computer science student at the University of Helsinki,
+                    currently pursuing my master&apos;s degree. 
+                    I am open to all possibilities in the tech field, but currently my expertise lies in agile and user-centered software development.
+                </p>
+                <p className="items-center justify-start text-xl md:text-2xl">
+                More about my experience in my CV:
+                </p>
+                <div className="my-7 sm:justify-center md:justify-start flex flex-col gap-5 text-5xl text-primary sm:flex-row">
+                <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://drive.google.com/file/d/1rszwL5F5OuKhJV3-JxG8Ge6mbUCCTIsA/view?usp=sharing"
+                        className="button-hover flex items-center justify-center rounded-lg border vibrant-border px-4 py-2 text-lg vibrant-text"
+                        >
+                        <TbFileCv className="mr-2 icon-primary" /> CV
+                    </a>
                 </div>
             </div>
         </div>
-      </div>
     )
   }
 
--- src/pages/Home/components/Projects.jsx
+++ src/pages/Home/components/Projects.jsx
@@ -7,78 +7,76 @@ const Projects = () => {
       className="py-28 text-white max-w-[1200px] mx-auto "id="projects"
     >
       <div className="px-4 mb-4 items-center gap-12 md:flex-col-reverse md:gap-6">
-        <div className="flex flex-col">
-          <h2 className="title-large ml-3">
-            My <span>Projects</span>
+          <h2 className="title-large ml-3 pb-5">
+          Projects
           </h2>
 
-          <div className="my-3 grid w-full grid-cols-1 gap-14 lg:grid-cols-2">
-            <Card
-              title="Traininghub"
-              description={
-                <>
-                <p className="mb-4">
-                On a Software Engineering Lab course, our software team developed a fullstack application for Eficode Oy&apos;s internal training purposes.
-                </p>
-                <p>
-                  We followed <strong>Scrum</strong> principles and used technologies such as <strong>Typescript</strong>, 
-                  <strong>React</strong>, <strong>Next.js</strong>, <strong>PostgreSQL</strong> and <strong>Cypress</strong>.
-                </p>
-                </>
-              }
-              link="https://youtu.be/AxFC3-6Vzls"
-              linkDescription="Demo Video"
-              link2="https://drive.google.com/file/d/1DcCkOTZ1NqCzyu6HnxKFL2GbmXq4Lec-/view?usp=sharing"
-              linkDescription2="Certificate"
-            />
-            <Card
-              title="HCI Project"
-              description={
-                <>
-                <p className="mb-4">
-                A project for the Human-Computer Interaction course at the University of Helsinki. 
-                Through this project, I gained hands-on experience in user-centered design by iterating on a mobile app prototype and conducting user research. 
-                </p>
-                <p>
-                I used <strong>Canva</strong> for prototyping.
-                </p>
-                </>
-              }
-              link="https://youtu.be/eAsj6y5yYl8"
-              linkDescription="Prototype"
-              link2="https://docs.google.com/document/d/1TRG1CH2AAgGD048RMWQSMaglW61VR-R9qKf9OvJBv6Q/edit?usp=sharing"
-              linkDescription2="Report"
-            />
-            <Card
-              title="Maze Generator"
-              description={
-                <>
-                <p className="mb-4">
-                A project for the 2023 Data Structures Lab at the University of Helsinki, implementing and visualizing Kruskal’s and Depth-First Search algorithms for maze generation.                </p>
-                <p>
-                I built the algorithms with <strong>Python</strong>, visualized them with <strong>Pygame</strong> and did unit testing with <strong>Pytest</strong>.              
-                </p>
-                </>
-              }
-              link="https://github.com/laurelcrelia/data-structures-lab"
-              linkDescription="Repository"
-            />
-            <Card
-              title="Work in progress..."
-              description={
-                <>
-                <p className="mb-4">
-                This is a placeholder for my other projects that I will be adding soon.
-                </p>
-                <p>
-                Most of my coding projects are on my repositories on GitHub so feel free to check them out:
-                </p>
-                </>
-              }
-              link="https://github.com/laurelcrelia?tab=repositories"
-              linkDescription="Github"
-            />
-          </div>
+        <div className=" flex-col my-3 grid w-full grid-cols-1 gap-14 lg:grid-cols-2">
+          <Card
+            title="Traininghub"
+            description={
+              <>
+              <p className="mb-4">
+              On a Software Engineering Lab course, our software team developed a fullstack application for Eficode Oy&apos;s internal training purposes.
+              </p>
+              <p>
+                We followed <strong>Scrum</strong> principles and used technologies such as <strong>Typescript</strong>, 
+                <strong>React</strong>, <strong>Next.js</strong>, <strong>PostgreSQL</strong> and <strong>Cypress</strong>.
+              </p>
+              </>
+            }
+            link="https://youtu.be/AxFC3-6Vzls"
+            linkDescription="Demo Video"
+            link2="https://drive.google.com/file/d/1DcCkOTZ1NqCzyu6HnxKFL2GbmXq4Lec-/view?usp=sharing"
+            linkDescription2="Certificate"
+          />
+          <Card
+            title="HCI Project"
+            description={
+              <>
+              <p className="mb-4">
+              A project for the Human-Computer Interaction course at the University of Helsinki. 
+              Through this project, I gained hands-on experience in user-centered design by iterating on a mobile app prototype and conducting user research. 
+              </p>
+              <p>
+              I used <strong>Canva</strong> for prototyping.
+              </p>
+              </>
+            }
+            link="https://youtu.be/eAsj6y5yYl8"
+            linkDescription="Prototype"
+            link2="https://docs.google.com/document/d/1TRG1CH2AAgGD048RMWQSMaglW61VR-R9qKf9OvJBv6Q/edit?usp=sharing"
+            linkDescription2="Report"
+          />
+          <Card
+            title="Maze Generator"
+            description={
+              <>
+              <p className="mb-4">
+              A project for the 2023 Data Structures Lab at the University of Helsinki, implementing and visualizing Kruskal’s and Depth-First Search algorithms for maze generation.                </p>
+              <p>
+              I built the algorithms with <strong>Python</strong>, visualized them with <strong>Pygame</strong> and did unit testing with <strong>Pytest</strong>.              
+              </p>
+              </>
+            }
+            link="https://github.com/laurelcrelia/data-structures-lab"
+            linkDescription="Repository"
+          />
+          <Card
+            title="Work in progress..."
+            description={
+              <>
+              <p className="mb-4">
+              This is a placeholder for my other projects that I will be adding soon.
+              </p>
+              <p>
+              Most of my coding projects are on my repositories on GitHub so feel free to check them out:
+              </p>
+              </>
+            }
+            link="https://github.com/laurelcrelia?tab=repositories"
+            linkDescription="Github"
+          />
         </div>
       </div>
     </div>
